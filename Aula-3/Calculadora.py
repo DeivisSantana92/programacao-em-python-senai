49daf06ac8133e88b1d707727053d73cf27662fd
DeivisSantana92 <deivis.santana92@gmail.com>
2026-07-17 21:46:10 -0300

    Add files via upload

---
+++ Aula-3/Calculadora.py
@@ -0,0 +1,29 @@
+# ```python
+# n1 = 10
+# n2 = 5
+# soma  =  n1 + n2
+# print(soma)
+
+# print('--------------------------------')
+# print('Calculadora Python')
+
+# não pode utilizar condicionais
+# Utiliza sinais aritméticos
+# print / input()
+# concatenação
+# 4 operações básicas
+
+# Calculadora Python
+
+n1 = 10
+n2 = 5
+n3 = 8
+n4 = 9
+soma = n1 + n2
+print(soma)
+multiplicacao = n3 * n4
+print(multiplicacao)
+subtracao = n4 - n2
+print(subtracao)
+divisao = n1 // n2
+print(divisao) 
---
+++ Aula-3/main.py
@@ -0,0 +1,20 @@
+print('Hello,world')
+print(type(10))
+print(type(10+2))
+print(type("strings == textos"))
+print(10+2)
+print(10 +  2)
+print(10 -  2)
+print(10 *  2)
+print(10.0 /  2)
+print(10 //  2)
+print(10 % 2)
+print(16 ** 2)
+
+
+print(10 > 2) # maior ?
+print(10 < 2) # menor ?
+print(10 >= 2) # maior OU igual ?
+print(10 <= 2) # menor OU igual ?
+print(10 == 2) # é igual? 
+print(10 != 2) # é diferente?
